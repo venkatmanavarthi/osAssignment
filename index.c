d047d3b0194156f6671d6b42155e0bf40f3a15f3
venkatmanavarthi <manavarthivenkat@icloud.com>
2020-04-12 15:26:06 +0530

    added the bool library

--- index.c
+++ index.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
+#include<stdbool.h>
+//stdbool is used to import a syntax for a bool data-type
 
+int main(){
+  char students[] = {'a','b','c','d','e','f','g','h','i','j'};
+  int n = sizeof(students)/sizeof(students[0]) // this line of code will return the number of elements or length of an array
+  int numberOfGifts[n];
+  int i = 0;
+  //taking input from the user the number of gifts purchased by each student
+  for(i = 0;i < n;i++){
+  }
+  return 0;
+}
